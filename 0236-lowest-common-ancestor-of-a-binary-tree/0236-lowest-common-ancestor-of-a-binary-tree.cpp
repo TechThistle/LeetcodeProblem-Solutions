2d6c48ca08f1b2b657c56b1aa2fd201736c94361
Dimple <dimple4122004@gmail.com>
2026-06-24 15:27:53 +0530

    Time: 59 ms (33.94%), Space: 43.9 MB (21.47%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -12,7 +12,7 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root==NULL)
         {
-            return 0;
+            return NULL;
         }
         if(root==p || root==q)
         {
