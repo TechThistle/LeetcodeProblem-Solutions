93c232669a07dae20047ab7efbc92bb569b9d963
Dimple <dimple4122004@gmail.com>
2026-07-23 22:39:11 +0530

    Time: 0 ms (100%), Space: 11.6 MB (79.31%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        int mid=0;
+        while(mid<=high)
+        {
+        if(nums[mid]==0)
+        {
+            swap(nums[low],nums[mid]);
+            low++;
+            mid++;
+        }
+        else if(nums[mid]==2)
+        {
+            swap(nums[high],nums[mid]);
+            high--;
+        }
+        else
+        {
+            mid++;
+        }}
         
     }
 };
