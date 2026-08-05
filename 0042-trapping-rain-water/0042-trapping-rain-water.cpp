0561ae3174bc605fba36aeac52369f00d3b4a145
Dimple <dimple4122004@gmail.com>
2026-08-05 22:39:07 +0530

    Time: 0 ms (100%), Space: 25.9 MB (78.31%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        int l=0;
+        int r=n-1;
+        int water=0;
+        int lmax=0;
+        int rmax=0;
+        while(l<r)
+        {
+           lmax=max(lmax, height[l]);
+           rmax=max(rmax, height[r]);
+           if(lmax<rmax)
+           {
+            water+=lmax-height[l];
+            l++;
+           } 
+           else
+           {
+            water+=rmax-height[r];
+            r--;
+           }
+        }
+        return water;
+    }
+};
